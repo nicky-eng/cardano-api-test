0f3872b1e24dc5ee440fa432ae554f3ed7895e87
nicky-eng <nicolasoyarzabal@gmail.com>
2021-07-19 20:35:37 -0300

    Added html template

--- stores/views.py
+++ stores/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Store
 
 
 def index(request):
     store_list = Store.objects.all()
-    output = ",".join([store.name for store in store_list])
-    return HttpResponse(output)
+    template = loader.get_template("stores/index.html")
+    context = {
+        "store_list": store_list,
+    }
+    return HttpResponse(template.render(context, request))
